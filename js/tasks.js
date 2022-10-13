684c1330c7e5a68679fa66988f0c7c69ef2f0b68
Michail <Gismanus@gmail.com>
2022-10-13 17:24:51 +0300

    exercises

--- js/tasks.js
+++ js/tasks.js
@@ -1,10 +1,20 @@
 // 2
 
-let styles = ['Jazz', 'Blues'];
+/* let styles = ['Jazz', 'Blues'];
 styles.push('Rock-n-Roll');
 styles[Math.floor(styles.length / 2)] = 'Classic';
 console.log(styles);
 console.log(styles.shift());
 console.log(styles);
 styles.unshift(...['Rap','Raggie']);
-console.log(styles);
+console.log(styles); */
+
+// 3
+
+let arr = ['a', 'b'];
+
+arr.push(function(){
+    console.log(this);
+})
+
+arr[2](); // ['a', 'b', function];
