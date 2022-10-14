bde54480f40b3d34bf05546ec9ec1237a7b0bbed
Michail <Gismanus@gmail.com>
2022-10-14 17:23:57 +0300

    exercises

--- js/tasks.js
+++ js/tasks.js
@@ -11,10 +11,33 @@ console.log(styles); */
 
 // 3
 
-let arr = ['a', 'b'];
+/* let arr = ['a', 'b'];
 
 arr.push(function(){
     console.log(this);
 })
 
-arr[2](); // ['a', 'b', function];
+arr[2](); // ['a', 'b', function]; */
+
+function sumInput() {
+    let arr = [];
+    let sum = 0;
+    function enterValue() {
+        return prompt('Enter value', ''); // string for ease
+    }
+    while (true) {
+        value = enterValue();
+        if (value != null){
+            value = +value;
+        }
+        if(isNaN(value) || value == null) break;
+        arr.push(value);
+        console.log(arr);
+    };
+    for (let key of arr) {
+        sum += key;
+    }
+    return sum;
+}
+
+console.log(sumInput());
--- js/test.js
+++ js/test.js
@@ -1,3 +1,3 @@
-let array = [1, 3, 4, 5];
-console.log(array[array.length - 1]);
-console.log(array.at(-1)); // is no supported by node.js
+let lol = prompt('lol', '');
+console.log(lol);
+console.log(+lol);
